189397024acb4954bb51294bed1d8e3a4cb31327
Sooji Kim <yh0gogo@gmail.com>
2022-09-29 13:15:18 +0900

    Update: path alias 추가

--- webpack.common.js
+++ webpack.common.js
@@ -9,8 +9,10 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     modules: [path.resolve(__dirname, 'src'), 'node_modules'],
     alias: {
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
       '@styles': path.resolve(__dirname, './src/styles'),
+      '@type': path.resolve(__dirname, './src/types'),
     },
   },
   module: {
