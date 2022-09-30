c0bffe700214696b66f65b183e05892ab71d3fea
Sooji Kim <yh0gogo@gmail.com>
2022-09-30 13:53:24 +0900

    Update: 별점 평가 enum 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,11 @@ export const enum DIRECTION_TYPE {
   HORIZONTAL = 'horizontal',
   VERTICAL = 'vertical',
 }
+
+export const enum RATINGS {
+  ONE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+}
