9454f84bad80b173689ad8aa2dbb7021c62bd16c
Sooji Kim <yh0gogo@gmail.com>
2022-09-30 18:00:33 +0900

    Update: 폼 형식별로 화면에 노출

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Card from '@components/Card';
+import Form from '@components/Form';
 import GlobalStyle from '@styles/globalStyle';
 import { DIRECTION_TYPE, RATINGS } from '@type/index';
 import styled from 'styled-components';
 
 function App() {
+  const handleSubmit = () => {
+    console.log('submit event log');
+  };
+
   return (
     <>
       <GlobalStyle />
       <StyledSection>
+        <StyledName>Vertical card</StyledName>
         <StyledVerticalCard
           description={
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -21,7 +27,7 @@ function App() {
           rating={RATINGS.THREE}
           title={'Title'}
         />
-
+        <StyledName>Horizontal card</StyledName>
         <StyledHorizontalCard
           description={
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -32,6 +38,15 @@ function App() {
           reviewer={'John Doe'}
           title={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
         />
+      </StyledSection>
+
+      <StyledSection>
+        <StyledName>Writable form</StyledName>
+        <StyledForm defaultValue={'initial value'} maxTextLength={500} onSubmit={handleSubmit} />
+        <StyledName>Disabled form</StyledName>
+        <StyledForm disabled maxTextLength={500} />
+        <StyledName>Readonly form</StyledName>
+        <StyledForm defaultValue={'initial value'} readOnly maxTextLength={500} />
       </StyledSection>
     </>
   );
@@ -48,6 +63,17 @@ const StyledVerticalCard = styled(Card)`
 
 const StyledHorizontalCard = styled(Card)`
   width: 50%;
+`;
+
+const StyledName = styled.span`
+  color: #e54a34;
+  font-size: 1.6rem;
+  font-weight: 500;
+`;
+
+const StyledForm = styled(Form)`
+  width: 50%;
+  margin-bottom: 2rem;
 `;
 
 export default App;
